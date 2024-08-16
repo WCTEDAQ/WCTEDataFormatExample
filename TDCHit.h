af097baddbfb534c08f6a177abfdf9552650ca9b
Evgenii Zhemchugov <evgenii.zhemchugov@warwick.ac.uk>
2024-08-16 18:04:01 +0100

    TDCHit: fix invalid extended trigger time tag storage and interpretation
    Extended trigger time tag is not more significant bits of a time
    measurement. Instead, it is the time of the beginning of the trigger
    window sampled from a clock of lower frequency (25 ns vs 25 pcs). Also
    its least significant bits are stored in the trailer GEO address.
    
    Delete
      TDCHit::GetValue
      TDCHit::SetValue
    Rename
      TDCHit::GetMeasurement -> TDCHit::GetValue
      TDCHit::SetMeasurement -> TDCHit::SetValue
    
    TDCHit byte structure is almost completely rearranged and is not
    backwards compatible

--- TDCHit.h
+++ TDCHit.h
@@ -11,105 +11,111 @@ public:
     std::fill(std::begin(data), std::end(data), 0);
   };
   
-  TDCHit(uint32_t header, uint32_t measurement, uint32_t extended_time) {
-    std::fill(std::begin(data), std::end(data), 0);
-    SetGEO(bits<0, 4>(header));
-    SetEvent(bits<5, 26>(header));
-    SetMeasurement(bits<0, 20>(measurement));
-    SetChannel(bits<21, 25>(measurement));
-    SetTrailing(bits<26, 26>(measurement));
-    SetExtendedTime(bits<0, 26>(extended_time));
-  };
-  
-  uint32_t GetMeasurement() const {
-    return bits<0, 20>(data);
-  };
-  
-  void SetMeasurement(uint32_t measurement) {
-    set_bits<0, 20>(data, measurement);
-  };
-  
-  uint32_t GetExtendedTime() const {
-    return bits<21, 47>(data);
-  };
-  
-  void SetExtendedTime(uint32_t time) {
-    set_bits<21, 47>(data, time);
+  TDCHit(
+      uint32_t header,
+      uint32_t measurement,
+      uint32_t extended_time,
+      uint32_t trailer
+  ) {
+    data[6] = 0;
+    init(header, measurement);
+    SetExtendedTime(bits<0, 26>(extended_time) << 5 | bits<0, 4>(trailer));
   };
 
-  uint64_t GetValue() const {
-    return bits<0, 47>(data);
+  // When extended trigger time tag is disabled, trailer is not needed
+  TDCHit(uint32_t header, uint32_t measurement) {
+    std::fill(data + 6, std::end(data), 0);
+    init(header, measurement);
   };
-  
-  void SetValue(uint64_t value) {
-    set_bits<0, 47>(data, value);
+
+  uint32_t GetValue() const {
+    return bits<0, 20>(data);
   };
-  
-  uint32_t GetEvent() const {
-    return bits<48, 69>(data);
-  };
-  
-  void SetEvent(uint32_t event) {
-    set_bits<48, 69>(data, event);
-  };
-  
-  uint16_t GetChannel() const {
-    return bits<72, 76>(data);
-  };
-  
-  void SetChannel(uint8_t channel) {
-    set_bits<72, 76>(data, channel);
-  };
-  
-  bool GetTrailing() const {
-    return bits<77, 77>(data);
-  };
-  
-  void SetTrailing(bool trailing) {
-    set_bits<77, 77>(data, trailing);
+
+  void SetValue(uint32_t value) {
+    set_bits<0, 20>(data, value);
   };
 
   uint8_t GetGEO() const {
-    return bits<80, 84>(data);
+    return bits<21, 25>(data);
   };
-  
+
   void SetGEO(uint8_t geo) {
-    set_bits<80, 84>(data, geo);
+    set_bits<21, 25>(data, geo);
   };
-  
+
+  bool GetTrailing() const {
+    return bits<26, 26>(data);
+  };
+
+  void SetTrailing(bool trailing) {
+    set_bits<26, 26>(data, trailing);
+  };
+
+  uint8_t GetChannel() const {
+    return bits<27, 31>(data);
+  };
+
+  void SetChannel(uint8_t channel) {
+    set_bits<27, 31>(data, channel);
+  };
+
+  uint32_t GetEvent() const {
+    return bits<32, 53>(data);
+  };
+
+  void SetEvent(uint32_t event) {
+    set_bits<32, 53>(data, event);
+  };
+
+  uint32_t GetExtendedTime() const {
+    return bits<56, 87>(data);
+  };
+
+  void SetExtendedTime(uint32_t time) {
+    set_bits<56, 87>(data, time);
+  };
+
   void Print(std::ostream& output = std::cout) const {
-    output << "measurement = " << GetMeasurement() << std::endl;
-    output << "extended_time = " << GetExtendedTime() << std::endl;
     output << "value = " << GetValue() << std::endl;
-    output << "event = " << GetEvent() << std::endl;
-    output << "channel = " << GetChannel() << std::endl;
-    output << "trailing = " << GetTrailing() << std::endl;
     output << "geo = " << static_cast<int>(GetGEO()) << std::endl;
+    output << "trailing = " << GetTrailing() << std::endl;
+    output << "channel = " << GetChannel() << std::endl;
+    output << "event = " << GetEvent() << std::endl;
+    output << "extended_time = " << GetExtendedTime() << std::endl;
   };
 
 private:
   /* bits     width   field
-   *  0 - 20  21      measurement
-   * 21 - 47  27      extended time
-   * 48 - 69  22      event
-   * 72 - 76   5      channel
-   * 77 - 77   1      trailing
-   * 80 - 84   5      geo
+   *  0 - 20  21      value
+   * 21 - 25   5      geo
+   * 26 - 26   1      trailing
+   * 27 - 31   5      channel
+   * 32 - 53  22      event
+   * 56 - 87  32      extended trigger time tag
    
       01234567
-    0 mmmmmmmm         0 = zero
-    1 mmmmmmmm         m = measurement
-    2 mmmmmTTT         c = channel
-    3 TTTTTTTT         t = trailing
-    4 TTTTTTTT         C = event
-    5 TTTTTTTT         T = extended time
-    6 CCCCCCCC
-    7 CCCCCCCC
-    8 CCCCCC00
-    9 ccccct00
-   10 ggggg000
+    0 vvvvvvvv          0 = reserved
+    1 vvvvvvvv          v = value
+    2 vvvvvggg          g = geo
+    3 ggtccccc          t = trailing
+    4 EEEEEEEE          c = channel
+    5 EEEEEEEE          E = event
+    6 EEEEEE00          T = extended trigger time tag
+    7 TTTTTTTT
+    8 TTTTTTTT
+    9 TTTTTTTT
+   10 TTTTTTTT
   */
   uint8_t data[11];
+
+  void init(uint32_t header, uint32_t measurement) {
+    SetGEO(bits<0, 4>(header));
+    SetEvent(bits<5, 26>(header));
+    SetValue(bits<0, 20>(measurement));
+    SetChannel(bits<21, 25>(measurement));
+    SetTrailing(bits<26, 26>(measurement));
+  };
 };
 
 
